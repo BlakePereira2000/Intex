1f7d98b129165eb8078195d1a5077ece7bbe7ca0
CameronPike <cp475@byu.edu>
2022-11-30 19:40:49 -0700

    journal overlay functionality

--- intexapp/urls.py
+++ intexapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import indexPageView, aboutPageView,journalPageView, reportPageView, savesignupView
 from .views import userPageView, foodsPageView, loginPageView, signupPageView, authenticate,logoutView, apiSearchPageView, addNewFoodPageView
-from .views import add_food_to_dayPageView,food_db_searchView, save_food_to_dayView, updateUserPageView
+from .views import add_food_to_dayPageView,food_db_searchView, save_food_to_dayView, updateUserPageView,updateDailyStatsPageView
+from .views import updateWaterPageView, updateLabPageView
 
 urlpatterns = [
     path('', indexPageView, name='index'),
@@ -21,4 +22,7 @@ urlpatterns = [
     path('food_db_search/', food_db_searchView, name='food_db_search'),
     path('save_food_to_day/', save_food_to_dayView, name='save_food_to_day'),
     path('update_user/', updateUserPageView, name='update_user'),
+    path('update_daily_stats/', updateDailyStatsPageView, name='update_daily_stats'),
+    path('update_water/', updateWaterPageView, name='update_water'),
+    path('update_lab/', updateLabPageView, name='update_lab'),
 ]
