d683294f9f8c49eaf1697b5b0f1aded8668bc305
edemaere <demaere.ezra@gmail.com>
2022-12-01 20:23:35 -0700

    adding cam's commits

--- intexProject/settings.py
+++ intexProject/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'kidney',
         'USER': 'postgres',
-        'PASSWORD': 'password',
+        'PASSWORD': '1234',
         'HOST':'localhost',
         'PORT': 5432
     }
